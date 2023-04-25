a0b6b4caaac9b74c1373c5c764e344bb87eb2ee7
Jhon Daniel <pacodan3@gmail.com>
2023-04-25 15:25:51 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,25 @@
+#!/bin/bash
+
+# Update existing list of packages
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+# Install prerequisite packages
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add GPG key for the official Docker repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-#Add the Docker repository to APT sources:
+
+# Add Docker repository to APT sources
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update existing list of packages again
 sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Check Docker version
+sudo docker version
+
+# Check Docker installation with hello-world
+sudo docker run hello-world
