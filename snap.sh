af8aeff7e71b1e0d3da894b4d064a968e0b1466f
Jhon Daniel <pacodan3@gmail.com>
2023-04-27 01:31:17 -0300

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -1,4 +1,4 @@
-sudo mv /etc/apt/preferences.d/nosnap.pref ~/Documents/nosnap.backup
+#sudo mv /etc/apt/preferences.d/nosnap.pref ~/Documents/nosnap.backup
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt update
 sudo apt install snapd
