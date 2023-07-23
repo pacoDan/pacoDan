67bf21e2a2ec7d759eefe22b08c9224b927d82dc
Jhon Daniel <pacodan3@gmail.com>
2023-07-23 20:06:03 -0300

    Update bienvenida_terminal.sh

--- bienvenida_terminal.sh
+++ bienvenida_terminal.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-# Art ASCII del mensaje de bienvenida
-art_ascii=$(cat << "EOF"
-     ____                                 ____
-   / __ )__  _____  ____  ____ ______   / / /
-  / __  / / / / _ \/ __ \/ __ `/ ___/  / / / 
- / /_/ / /_/ /  __/ / / / /_/ (__  )  /_/_/  
-/_____/\__,_/\___/_/ /_/\__,_/____/  (_|_)   
-                                             
-       ____                   ____              _      __
-      / / /_  ____  ____     / __ \____ _____  (_)__  / /
- __  / / __ \/ __ \/ __ \   / / / / __ `/ __ \/ / _ \/ / 
-/ /_/ / / / / /_/ / / / /  / /_/ / /_/ / / / / /  __/ /  
-\____/_/ /_/\____/_/ /_/  /_____/\__,_/_/ /_/_/\___/_/   
-EOF
-)
-
 # Ruta del archivo .zshrc
 zshrc_file=~/.zshrc
 
-# Comprobar si el arte ASCII ya existe en el archivo .zshrc
-if grep -q "$art_ascii" "$zshrc_file"; then
+# URL del archivo con el arte ASCII del mensaje de bienvenida
+welcome_ascii_url="https://raw.githubusercontent.com/pacodan/pacodan/main/ascii_bienvenida.txt"
+
+# Descargar el archivo welcome_ascii.txt
+wget -O /tmp/welcome_ascii.txt "$welcome_ascii_url"
+
+# Verificar si la descarga del archivo fue exitosa
+if [ $? -ne 0 ]; then
+  echo "ERROR: No se pudo descargar el archivo welcome_ascii.txt. Verifica la URL y tu conexión a Internet."
+  exit 1
+fi
+
+# Verificar si el arte ASCII ya existe en el archivo .zshrc
+if grep -q "$welcome_ascii_url" "$zshrc_file"; then
   echo "Mensaje de bienvenida ya existe en el archivo .zshrc. No se agregará nuevamente."
 else
   # Eliminar el arte ASCII anterior (si existe)
-  sed -i '/^     ____                                 ____.*/,$d' "$zshrc_file"
+  sed -i '/^# Mensaje de bienvenida personalizado.*/,/^# Fin del mensaje de bienvenida personalizado./d' "$zshrc_file"
 
   # Agregar el arte ASCII al archivo .zshrc
-  echo "$art_ascii" >> "$zshrc_file"
+  echo "# Mensaje de bienvenida personalizado." >> "$zshrc_file"
+  cat /tmp/welcome_ascii.txt >> "$zshrc_file"
+  echo "# Fin del mensaje de bienvenida personalizado." >> "$zshrc_file"
+
   echo "Mensaje de bienvenida personalizado agregado al archivo .zshrc!"
 fi
+
+# Eliminar el archivo temporal
+rm /tmp/welcome_ascii.txt
