f95e1cdb601b20a1dc069eb7f2af0243e67c14b2
Jhon Daniel <pacodan3@gmail.com>
2023-07-23 17:29:35 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Función para manejar la entrada de contraseña para sudo
-ingresar_contraseña_sudo() {
-  sudo -Sv <<< "$1"
-}
-
 # Verificar si se necesita sudo probando si apt se puede actualizar sin contraseña
 if sudo -n true; then
   # Si sudo no requiere una contraseña, actualizar sin solicitar contraseña
@@ -12,11 +7,10 @@ if sudo -n true; then
   sudo apt install -y --yes zsh gcc make cmake xsel wl-clipboard ripgrep unzip neofetch screenfetch gettext g++
 else
   # Si sudo requiere una contraseña, solicitarla al principio
-  read -sp "Ingresa tu contraseña de sudo: " contraseña_sudo
-  echo "$contraseña_sudo" | ingresar_contraseña_sudo "$contraseña_sudo"
+  read -sp "Ingresa tu contrasena de sudo: " contrasena_sudo
+  echo $contrasena_sudo | sudo -S true
   sudo apt update -y
-  echo "$contraseña_sudo" | ingresar_contraseña_sudo "$contraseña_sudo"
-  sudo apt install -y --yes zsh gcc make cmake xsel wl-clipboard ripgrep unzip neofetch screenfetch gettext g++
+  echo $contrasena_sudo | sudo -S apt install -y --yes zsh gcc make cmake xsel wl-clipboard ripgrep unzip neofetch screenfetch gettext g++
 fi
 
 # Resto del script sigue igual
