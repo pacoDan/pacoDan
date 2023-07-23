fcf4a1fbd90501acdf3df2a1663bf038b25e8631
Jhon Daniel <pacodan3@gmail.com>
2023-07-23 16:58:01 -0300

    script de instalacion de libs utiles y personalizacion de la terminal

---
+++ script_arch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Instalar Zsh
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm zsh gcc make cmake xsel wl-clipboard ripgrep unzip neofetch screenfetch gettext
+
+# Cambiar la shell actual a Zsh
+chsh -s $(which zsh)
+
+# Instalar Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
+# Cambiar el tema de Oh My Zsh a Powerlevel10k
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' $HOME/.zshrc
+
+# Reiniciar la shell
+exec zsh
