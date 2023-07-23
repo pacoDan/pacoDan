b1ba01baf40fba95fbc19c45ed30622eecc89fd1
Jhon Daniel <pacodan3@gmail.com>
2023-07-23 17:26:08 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# Ask for the sudo password and extend the sudo timeout
-sudo -v
+# Función para manejar la entrada de contraseña para sudo
+ingresar_contraseña_sudo() {
+  sudo -Sv <<< "$1"
+}
 
-# Instalar Zsh
-sudo apt update -y
-sudo apt install -y --yes zsh gcc make cmake xsel wl-clipboard ripgrep unzip neofetch screenfetch gettext g++
+# Verificar si se necesita sudo probando si apt se puede actualizar sin contraseña
+if sudo -n true; then
+  # Si sudo no requiere una contraseña, actualizar sin solicitar contraseña
+  sudo apt update -y
+  sudo apt install -y --yes zsh gcc make cmake xsel wl-clipboard ripgrep unzip neofetch screenfetch gettext g++
+else
+  # Si sudo requiere una contraseña, solicitarla al principio
+  read -sp "Ingresa tu contraseña de sudo: " contraseña_sudo
+  echo "$contraseña_sudo" | ingresar_contraseña_sudo "$contraseña_sudo"
+  sudo apt update -y
+  echo "$contraseña_sudo" | ingresar_contraseña_sudo "$contraseña_sudo"
+  sudo apt install -y --yes zsh gcc make cmake xsel wl-clipboard ripgrep unzip neofetch screenfetch gettext g++
+fi
 
-# Cambiar la shell actual a Zsh
-chsh -s $(which zsh)
-
-# Instalar Oh My Zsh
+# Resto del script sigue igual
+chsh -s "$(which zsh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Instalar Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-
-# Cambiar el tema de Oh My Zsh a Powerlevel10k
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' $HOME/.zshrc
-
-# Reiniciar la shell
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' "$HOME/.zshrc"
 exec zsh
