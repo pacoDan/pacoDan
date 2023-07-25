93877188ad7fd09f5147fdb2b1216663898261bc
Jhon Daniel <pacodan3@gmail.com>
2023-07-24 21:32:23 -0300

    Update personalizar_zsh.sh

--- personalizar_zsh.sh
+++ personalizar_zsh.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Instalar zsh-syntax-highlighting si no está instalado
-echo "Instalando zsh-syntax-highlighting..."
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install fzf with automatic confirmations
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all --key-bindings --no-update-rc
+
+# Update the zshrc file with the desired plugins
+if ! grep -q "plugins=(" ~/.zshrc; then
+  sed -i '/^plugins=(/a \ \ zsh-autosuggestions\n)' ~/.zshrc
 fi
 
-# Instalar zsh-autosuggestions si no está instalado
-echo "Instalando zsh-autosuggestions..."
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
-
-# Configurar plugins en ~/.zshrc
-echo "Configurando plugins en ~/.zshrc..."
-plugins_line="plugins=(git zsh-syntax-highlighting zsh-autosuggestions)"
-if ! grep -q "$plugins_line" ~/.zshrc; then
-    echo "$plugins_line" >> ~/.zshrc
-fi
-
-# Reiniciar la terminal
-echo "Reiniciando la terminal..."
-exec zsh
+echo "Plugin zsh-autosuggestions has been installed and added to your ~/.zshrc file."
+echo "FZF has been installed with automatic confirmations."
+echo "Please restart your shell for the changes to take effect."
