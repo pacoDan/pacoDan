15f65908f0c881ae02da48e76bab0bb505c9f64e
Jhon Daniel <pacodan3@gmail.com>
2024-08-09 14:25:38 -0300

    Update install_nvim.sh

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-# Clonar el repositorio de neovim
-git clone https://github.com/neovim/neovim.git
+# Ruta donde se clonará el repositorio temporalmente
+TMP_DIR="/tmp/nvim"
 
-# Ingresar al directorio neovim
-cd neovim
+# Eliminar el directorio temporal si ya existe
+rm -rf $TMP_DIR
 
-# Cambiar a la rama release-0.9
-git checkout release-0.9
+# Clonar el repositorio de Neovim desde la rama master
+git clone --branch master https://github.com/neovim/neovim.git $TMP_DIR
 
-# Compilar e instalar neovim
+# Entrar al directorio clonado
+cd $TMP_DIR
+
+# Compilar Neovim
 make CMAKE_BUILD_TYPE=Release
-sudo make install
-cd
-git clone https://github.com/LunarVim/nvim-basic-ide.git ~/.config/nvim
+
+# Instalar Neovim
+make install
+
+# Mover los binarios y archivos necesarios a /root/.desarrollo
+mkdir -p /root/.desarrollo/nvim-master
+cp -r build/* /root/.desarrollo/nvim-master/
+
+# Crear un enlace simbólico en /usr/local/bin para que nvim esté en el PATH
+ln -sf /root/.desarrollo/nvim-master/bin/nvim /usr/local/bin/nvim
+
+# Limpiar el directorio temporal
+rm -rf $TMP_DIR
+
+echo "Neovim se ha instalado correctamente desde la rama master y está disponible en el PATH global."
